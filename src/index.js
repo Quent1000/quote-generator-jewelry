a6535ef65e0b2ac0aebf16dd75c8ad4c81918097
Quent1000 <imhoff.quentin@orange.fr>
2024-09-25 21:32:57 +0200

    Mise à jour des dépendances et configuration pour le déploiement Vercel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';  // Mettez à jour ce chemin si nécessaire
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';  // Assurez-vous que cette ligne pointe vers le bon fichier
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
